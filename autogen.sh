8e0656501c27faad2818aaf3ec0ddc97e3bd3358
Johannes Zellner <webmaster@nebulon.de>
2005-09-07 04:38:28 +0000

    (Old svn revision: 777)

--- autogen.sh
+++ autogen.sh
@@ -1,47 +1,23 @@
-#! /bin/sh
-
-# $Id: autogen.sh,v 1.1 2005/01/26 22:28:25 nebulon Exp $
+#!/bin/sh
 #
-# Copyright (c) 2002  Daniel Elstner  <daniel.elstner@gmx.net>
+# $Id$
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License VERSION 2 as
-# published by the Free Software Foundation.  You are not allowed to
-# use any other version of the license; unless you got the explicit
-# permission from the author to do so.
+# Copyright (c) 2002-2005
+#         The Xfce development team. All rights reserved.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# Written for Xfce by Benedikt Meurer <benny@xfce.org>.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+(type xdt-autogen) >/dev/null 2>&1 || {
+  cat >&2 <<EOF
+autogen.sh: You don't seem to have the Xfce development tools installed on
+            your system, which are required to build this software.
+            Please install the xfce4-dev-tools package first, it is available
+            from http://www.xfce.org/.
+EOF
+  exit 1
+}
 
-#dir=`echo "$0" | sed 's,[^/]*$,,'`
-#test "x${dir}" = "x" && dir='.'
+exec xdt-autogen $@
 
-#if test "x`cd "${dir}" 2>/dev/null && pwd`" != "x`pwd`"
-#then
-#    echo "This script must be executed directly from the source directory."
-#    exit 1
-#fi
-
-#rm -f config.cache acconfig.h
-
-#echo "- libtoolize."		&& \
-#libtoolize --force		&& \
-echo "- aclocal."		&& \
-aclocal				&& \
-echo "- autoconf."		&& \
-autoconf			&& \
-#echo "- autoheader."		&& \
-#autoheader			&& \
-echo "- automake."		&& \
-automake --add-missing --gnu	&& \
-echo				&& \
-
-exit 1
-
+# vi:set ts=2 sw=2 et ai:
