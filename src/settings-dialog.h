7b26b767fa30dc2b30d4b3346cb75cbe73fe22ee
Simon Steinbeiss <simon.steinbeiss@elfenbeinturm.at>
2021-01-04 01:30:26 +0100

    Simplify settings dialog code

--- src/settings-dialog.h
+++ src/settings-dialog.h
@@ -17,17 +17,6 @@
 #include <glib-object.h>
 #include <gtk/gtk.h>
 
-#define XTM_TYPE_SETTINGS_DIALOG		(xtm_settings_dialog_get_type ())
-#define XTM_SETTINGS_DIALOG(obj)		(G_TYPE_CHECK_INSTANCE_CAST ((obj), XTM_TYPE_SETTINGS_DIALOG, XtmSettingsDialog))
-#define XTM_SETTINGS_DIALOG_CLASS(klass)	(G_TYPE_CHECK_CLASS_CAST ((klass), XTM_TYPE_SETTINGS_DIALOG, XtmSettingsDialogClass))
-#define XTM_IS_SETTINGS_DIALOG(obj)		(G_TYPE_CHECK_INSTANCE_TYPE ((obj), XTM_TYPE_SETTINGS_DIALOG))
-#define XTM_IS_SETTINGS_DIALOG_CLASS(klass)	(G_TYPE_CHECK_CLASS_TYPE ((klass), XTM_TYPE_SETTINGS_DIALOG))
-#define XTM_SETTINGS_DIALOG_GET_CLASS(obj)	(G_TYPE_INSTANCE_GET_CLASS ((obj), XTM_TYPE_SETTINGS_DIALOG, XtmSettingsDialogClass))
-
-typedef struct _XtmSettingsDialog XtmSettingsDialog;
-
-GType		xtm_settings_dialog_get_type			(void);
-GtkWidget *	xtm_settings_dialog_new				(GtkWindow *parent_window);
-void		xtm_settings_dialog_run				(XtmSettingsDialog *dialog);
+void		xtm_settings_dialog_run				(GtkWidget *parent_window);
 
 #endif /* !SETTINGS_DIALOG_H */
